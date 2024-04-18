b8661292e8b4c9979d912951b2f8e650bc4c8ca5
juli7julia7 <juli7julia7@gmail.com>
2024-04-18 17:26:22 +0300

    add section material

--- js/script.js
+++ js/script.js
@@ -9,3 +9,16 @@ function documentClick(e){
         document.documentElement.classList.toggle('menu-open');
     }
 }
+
+// =============== SLIDER ================
+const swiper = new Swiper('.slider-material', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  
+  });
